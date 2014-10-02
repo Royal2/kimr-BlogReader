6072e7fb9ac2aa1d3328c306c5e82dcb130be59b
Roy Kim <1997.roy@gmail.com>
2014-10-02 14:31:19 -0700

    Milestone S9

--- app/src/main/java/android/kimr/blogreader/BlogPostTask.java
+++ app/src/main/java/android/kimr/blogreader/BlogPostTask.java
@@ -2,12 +2,23 @@ package android.kimr.blogreader;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+//BlogPostTask inherits all the properties of AsyncTask.
 public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     @Override
-    protected JSONObject doInBackground(Activity... params) {
+    protected JSONObject doInBackground(Activity... activities) {
+        try {
+            URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count");
+        }
+        catch(MalformedURLException error){
+            Log.e("BlogPostTask", "Malformed URL: " + error);
+        }
         return null;
     }
 }
