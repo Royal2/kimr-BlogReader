9a2aa9a3ad05bb5d46dc3a151a9cf6f077715766
Roy Kim <1997.roy@gmail.com>
2014-10-02 14:49:46 -0700

    Milestone S10

--- app/src/main/java/android/kimr/blogreader/BlogPostTask.java
+++ app/src/main/java/android/kimr/blogreader/BlogPostTask.java
@@ -6,6 +6,8 @@ import android.util.Log;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -15,9 +17,20 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject> {
     protected JSONObject doInBackground(Activity... activities) {
         try {
             URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count");
+
+            HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+
+            if(responseCode == HttpURLConnection.HTTP_OK) {
+                 
+            }
         }
         catch(MalformedURLException error){
             Log.e("BlogPostTask", "Malformed URL: " + error);
+        }
+        catch (IOException error) {
+            Log.e("BlogPostTask", "IO Exception: " + error);
         }
         return null;
     }
