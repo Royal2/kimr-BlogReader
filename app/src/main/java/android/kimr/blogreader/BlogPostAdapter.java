ed874281c80b3b00b9a79b7a43c10034da4bd192
Roy Kim <1997.roy@gmail.com>
2014-10-20 15:04:09 -0700

    Milestone S31

--- app/src/main/java/android/kimr/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/kimr/blogreader/BlogPostAdapter.java
@@ -17,5 +17,8 @@ public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
     public View getView(int position, View convertView, ViewGroup parent) {
         BlogPost post = getItem(position);
 
+        if(convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
     }
 }
