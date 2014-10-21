a8955a756f226213614d8cdd945f5c6fe8bf1dad
Roy Kim <1997.roy@gmail.com>
2014-10-21 14:23:20 -0700

    Milestone S Free Milestone

--- app/src/main/java/android/kimr/blogreader/BlogPostParser.java
+++ app/src/main/java/android/kimr/blogreader/BlogPostParser.java
@@ -1,5 +1,6 @@
 package android.kimr.blogreader;
 
+import android.text.Html;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -59,7 +60,8 @@ public class BlogPostParser {
             for(int index = 0; index < jsonPosts.length(); index++) {
                 JSONObject post = jsonPosts.getJSONObject(index);
 
-                String title = post.getString("title");
+                //Using Html package to read Html code.
+                String title = Html.fromHtml(post.getString("title")).toString();
                 String url = post.getString("url");
 
                 BlogPost blogPost = new BlogPost(title, url);
