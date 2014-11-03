e2dbef57332e4213f49d65bd9e48de158b9a6b51
Roy Kim <1997.roy@gmail.com>
2014-11-03 14:51:18 -0800

    Milestone I7

--- app/src/main/java/android/kimr/blogreader/BlogWebActivity.java
+++ app/src/main/java/android/kimr/blogreader/BlogWebActivity.java
@@ -4,18 +4,33 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BlogWebActivity extends Activity {
+    protected ProgressBar progressBar;
+    protected WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blog_web);
 
+        progressBar = (ProgressBar) findViewById(R.id.progressBarWeb);
+        webView = (WebView) findViewById(R.id.webView);
+
         Intent intent = getIntent();
         Uri blogUri = intent.getData();
 
-        WebView webView = (WebView) findViewById(R.id.webView);
         webView.loadUrl(blogUri.toString());
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                //make progress bar disappear.
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
